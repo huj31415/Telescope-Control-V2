7517148e7900ae17efe0715c6de84b9f104e974a
huj31415 <huj31415@gmail.com>
2023-06-24 17:51:48 -0400

    Update telescopecontrolv2.ino
    stuff

--- telescopecontrolv2.ino
+++ telescopecontrolv2.ino
@@ -1,12 +1,15 @@
+#define debug_sidereal_planets
+
 #include <AccelStepper.h>
-#include <SiderealObjects.h>
 #include <SiderealPlanets.h>
+// #include <SiderealObjects.h>
+#include <avr/pgmspace.h>
 
 // Define the stepper control pins
-#define PitchDir 8
-#define PitchStep 9
-#define YawDir 16
-#define YawStep 10
+#define AltDir 8
+#define AltStep 9
+#define AzDir 16
+#define AzStep 10
 
 // Resolution pins (common)
 #define M0 5
@@ -14,13 +17,13 @@
 #define M2 3
 
 // Steps per revolution of the axle (default 200)
-#define StepsPerRevolution 200
+// #define StepsPerRevolution 200
 
-// Pitch gear ratio: 10:100
-#define PitchGearRatio 10
+// Alt gear ratio: 10:100
+#define AltGearRatio 10
 
-// Yaw gear ratio: 8:80
-#define YawGearRatio 10
+// Az gear ratio: 8:80
+#define AzGearRatio 10
 
 // Initial number of microsteps per step
 // Multiples of 2, 1 to 32
@@ -28,42 +31,36 @@
 #define MaxStepRes 32
 
 // Max speed in steps/sec
-#define maxSpeed 50 * MaxStepRes
+#define MaxSpeed 50 * MaxStepRes
 
 // Length of a sidereal day in seconds
 // #define SiderealDay 86164.0905
 
 // Joystick pins
 // X output
-#define joystickX A0
+#define JoystickX A0
 
 // Y output
-#define joystickY A1
+#define JoystickY A1
 
 // Button press
-#define joystickB 2
+#define JoystickB 2
 
-// Joystick detection threshold
-#define threshold 24
+// Joystick detection Threshold
+#define Threshold 24
 
 // Mode switch button pin
-#define modeBtn 7
-
-long long millisec;
-
-// // Joystick offsets compensate for drift
-// byte xOffset;
-// byte yOffset;
+#define ModeBtn 7 // for leonardo, 3 for uno
 
 // initialize the steppers
-AccelStepper az(AccelStepper::DRIVER, PitchStep, PitchDir);
-AccelStepper alt(AccelStepper::DRIVER, YawStep, YawDir);
+AccelStepper az(AccelStepper::DRIVER, AltStep, AltDir);
+AccelStepper alt(AccelStepper::DRIVER, AzStep, AzDir);
 
 // initialize sidereal objects and planets
-SiderealPlanets SPlanets;
-SiderealObjects SObjects;
+SiderealPlanets Planets;
+// SiderealObjects Stars;
 
-// possible states
+// possible program states
 enum State {
   COARSE,
   FINE,
@@ -98,9 +95,11 @@ struct Coord {
   // double set;
 };
 
+Coord planet;
+
 // Set the resolution (microsteps/step) of the steppers. The res pins are common to both so it is only done once.
 // Resolution is 1-32, multiples of 2.
-void setRes(byte res = 32) {
+void setRes(int res = 32) {
   int x = log(res) / log(2);
   digitalWrite(M0, (x & 1) == 0 ? LOW : HIGH);
   digitalWrite(M1, (x & 2) == 0 ? LOW : HIGH);
@@ -118,25 +117,26 @@ void changeRes() {
   }
 }
 
+
 // Read the x/y values of the joystick and move the motors accordingly
 void manualControl() {
 
   // read joystick values and set them to [-511, 512] instead of [0, 1023]
-  int x = (analogRead(joystickX) - 511);
-  int y = (analogRead(joystickY) - 511);
+  int x = (analogRead(JoystickX) - 511);
+  int y = (analogRead(JoystickY) - 511);
 
-  // Check if the joystick values are above the threshold to reduce drift or noise, then map + set speeds
-  if (abs(x) > threshold || abs(y) > threshold) {
+  // Check if the joystick values are above the Threshold to reduce drift or noise, then map + set speeds
+  if (abs(x) > Threshold || abs(y) > Threshold) {
     // Map joystick values to speeds
-    az.setSpeed(map(x, -512, 512, -maxSpeed, maxSpeed));
-    alt.setSpeed(map(y, -512, 512, -maxSpeed, maxSpeed));
+    az.setSpeed(map(x, -512, 512, -MaxSpeed, MaxSpeed));
+    alt.setSpeed(map(y, -512, 512, -MaxSpeed, MaxSpeed));
 
     // Move to implement acceleration (?)
     az.move(8);
     alt.move(8);
 
   } else {
-    // Ignore small variations below the threshold
+    // Ignore small variations below the Threshold
     az.stop();
     alt.stop();
     az.setSpeed(0);
@@ -149,66 +149,75 @@ void manualControl() {
 }
 
 void autoAim(Objects target) {
-  Coord planet;
+
+  // Planets.doPlans(target); // uses extra 10% program storage space - might be better for actual use
+  Serial.println("calcs done");
+
   switch (target) {
     // do other stuff like rise/set for each
     case SUN:
-      SPlanets.doSun();
+      Planets.doSun();
       //planets.doSunRiseSetTimes();
       break;
     case MOON:
-      SPlanets.doMoon();
+      Planets.doMoon();
       //planets.doMoonRiseSetTimes();
-      SPlanets.doLunarParallax();
+      //Planets.doLunarParallax(); // requires elevation
       break;
     case MERCURY:
-      SPlanets.doMercury();
+      Planets.doMercury();
       break;
     case VENUS:
-      SPlanets.doVenus();
+      Planets.doVenus();
       break;
     case MARS:
-      SPlanets.doMars();
+      Planets.doMars();
       break;
     case JUPITER:
-      SPlanets.doJupiter();
+      Planets.doJupiter();
       break;
     case SATURN:
-      SPlanets.doSaturn();
+      Planets.doSaturn();
       break;
     case URANUS:
-      SPlanets.doUranus();
+      Planets.doUranus();
       break;
     case NEPTUNE:
-      SPlanets.doNeptune();
+      Planets.doNeptune();
       break;
   }
-  planet.ra = SPlanets.getRAdec();
-  planet.dec = SPlanets.getDeclinationDec();
+  planet.ra = Planets.getRAdec();
+  planet.dec = Planets.getDeclinationDec();
 
-  SPlanets.setRAdec(planet.ra, planet.dec);
-  SPlanets.doRAdec2AltAz();
-  // planets.doRefractionC(pressure, temperature in C) // integrate BMP280 sensor to get these, for no refraction compensation for now
-  planet.alt = SPlanets.getAltitude();
-  planet.az = SPlanets.getAzimuth();
+  Serial.println("radec done");
 
-  Serial.print(F("Alt, Az: "));
+  Planets.setRAdec(planet.ra, planet.dec);
+  Planets.doRAdec2AltAz();
+  planet.alt = Planets.getAltitude();
+  planet.az = Planets.getAzimuth();
+
+  Serial.println("altaz done");
+
+  // Planets.setAltAz(planet.alt, planet.az);
+  // Planets.doRefractionC(pressure, temperature in C); // integrate BMP280 sensor to get these, for no refraction compensation for now
+
+  Serial.print("Alt, Az: ");
   Serial.print(planet.alt);
-  Serial.print(F(","));
+  Serial.print(",");
   Serial.println(planet.az);
 
   // after getting coords, ensure it is above horizon, then calculate distance between current position and target, then move accordingly (taking into account the gear ratios)
 }
 
-void setup()  // setup stuff
+void setup()
 {
   // set joystick pin modes to input
-  pinMode(joystickX, INPUT);
-  pinMode(joystickY, INPUT);
+  pinMode(JoystickX, INPUT);
+  pinMode(JoystickY, INPUT);
 
-  // set joystick button pin to input
-  pinMode(joystickB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(joystickB), changeRes, FALLING);
+  // set joystick button pin to input - only pins 2 and 3 are interrupt pins
+  pinMode(JoystickB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(JoystickB), changeRes, RISING);
 
   // init serial for debugging
   Serial.begin(9600);
@@ -217,36 +226,39 @@ void setup()  // setup stuff
   setRes(8);
 
   // set stepper max speed
-  alt.setMaxSpeed(maxSpeed);
-  az.setMaxSpeed(maxSpeed);
+  alt.setMaxSpeed(MaxSpeed);
+  az.setMaxSpeed(MaxSpeed);
 
   // set acceleration
   alt.setAcceleration(1);
   az.setAcceleration(1);
 
   // Init planets, integrate GPS module to get these
-  SPlanets.begin();
-  SPlanets.setTimeZone(-5);  // Relative to GMT - EST = GMT-5
-  SPlanets.useAutoDST();
+  Planets.begin();
+  Planets.setTimeZone(-5);  // Relative to GMT: EST = GMT-5
+  Planets.useAutoDST();
 
   // get these from gps module
-  SPlanets.setLatLong(41.8, -72.9);  // Avon, CT
-  SPlanets.setGMTdate(2023, 6, 20);
-  SPlanets.setGMTtime(19, 12, 0);
+  Planets.setLatLong(41.8, -72.9);  // Avon, CT
+  Planets.setGMTdate(2023, 6, 23);
+  Planets.setGMTtime(18, 9, 0);
 
   // for debugging and testing
   currentState = AUTOAIM;
 
-  autoAim(SUN); // for some reason venus, jupiter, saturn don't work
+  autoAim(MARS); // for some reason venus, jupiter, saturn don't work
 }
 
-void loop()  // loop
-{
+void loop()
+{/*
   if (currentState == COARSE || currentState == FINE) {
     // manualControl();
   } else if (currentState == AUTOAIM) {
-    // get target planet info
+    // get target planet info, then aim
   } else if (currentState == TRACK) {
-    // track
-  }
+    // aim then track either by calculating position every loop or aiming in a circle around polaris
+  }*/
+  while(1);
 }
+
+g
